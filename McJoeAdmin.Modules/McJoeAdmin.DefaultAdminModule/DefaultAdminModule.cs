a4c141bd7f846211fe06538963eb36ce8aa177f8
efess <efess@2f79f410-3f06-4843-a5cc-17fa70570e0a>
2011-02-14 04:28:27 +0000

    Moving modules

---
+++ McJoeAdmin.Modules/McJoeAdmin.DefaultAdminModule/DefaultAdminModule.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using McJoeAdmin.ModuleHost;
+using System.Timers;
+using McJoeAdmin.Model;
+
+namespace McJoeAdmin.DefaultAdminModule
+{
+    [CallbackBehavior(ConcurrencyMode = ConcurrencyMode.Reentrant)]
+    public class DefaultAdminModule : McModuleBase
+    {
+        private const int STATE_REFRESH_PERIOD = 5000;
+
+        // State vars
+        private List<string> _currentPlayers;
+
+        private Timer _timer = new Timer(STATE_REFRESH_PERIOD);
+
+        public DefaultAdminModule()
+        {
+            _currentPlayers = new List<string>();
+            _timer.Elapsed += (sender, e) => RefreshState();
+
+            _timer.Enabled = true;
+        }
+
+        // IMcAdminModule Implementation
+        private AdminRules _currentAdminRules;
+        public override void SetAdminRules(AdminRules pAdminRules)
+        {
+            _currentAdminRules = pAdminRules;
+        }
+
+        public override void MessageIn(McMessage pMessage)
+        {
+            var message = ProcessMessage(pMessage);
+        }
+
+        private McMessage ProcessMessage(McMessage pMessage)
+        {
+            var data = ReturnPlayerAndData(pMessage.Data);
+            if (pMessage.Data.Contains("Connected players: "))
+            {
+                ParsePlayers(pMessage.Data.Remove(0,19));
+            }
+            else if (!string.IsNullOrEmpty(data.Item1)
+                && data.Item2.ToUpper() == "!PLAYERS")
+            {
+                SendPlayerList(data.Item1);
+            }
+            return null;
+        }
+
+        private void ParsePlayers(string pPlayers)
+        {
+            lock (_currentPlayers)
+            {
+                _currentPlayers.Clear();
+                foreach (var player in pPlayers.Split(new char[] { ' ' }))
+                {
+                    _currentPlayers.Add(player.Trim());
+                }
+            }
+        }
+
+        public void SendPlayerList(string pPlayer)
+        {
+            SendMessage("tell " + pPlayer + " Current Players:");
+
+            for(int i = 0; i < _currentPlayers.Count; i++)
+            {
+                SendMessage("tell " + pPlayer + i + " " + _currentPlayers[i]);
+            }
+        }
+
+        public void RefreshState()
+        {
+            SendMessage("list");
+        }
+    }
+}

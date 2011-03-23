ff1cf98fd47d716d7c731b34c9b3442cb38d6c0a
efess <efess@2f79f410-3f06-4843-a5cc-17fa70570e0a>
2011-03-23 11:48:38 +0000

    Added dyes to calculator

--- McJoeAdmin.Modules/McJoeAdmin.MaterialCalculator/MaterialCalculator.cs
+++ McJoeAdmin.Modules/McJoeAdmin.MaterialCalculator/MaterialCalculator.cs
@@ -56,6 +56,59 @@ namespace McJoeAdmin.MaterialCalculator
                     case "!CALC":
                         DoStringCalc(message.SubMessage, message.Name);
                         break;
+                    case "!LADDERS":
+                        LaddersCalc(message.SubMessage, message.Name);
+                        break;
+                    case "!FENCES":
+                        FencesCalc(message.SubMessage, message.Name);
+                        break;
+                    case "!ARROWS":
+                        ArrowsCalc(message.SubMessage, message.Name);
+                        break;
+
+                    // DYES
+                    case "!LIGHTGRAY":
+                    case "!LIGHTGREY":
+                        LightGrayCalc(message.SubMessage, message.Name);
+                        break;
+                    case "!GRAY":
+                    case "!GREY":
+                        GrayCalc(message.SubMessage, message.Name);
+                        break;
+                    case "!RED":
+                        RedCalc(message.SubMessage, message.Name);
+                        break;
+                    case "!ORANGE":
+                        OrangeCalc(message.SubMessage, message.Name);
+                        break;
+                    case "!YELLOW":
+                        YellowCalc(message.SubMessage, message.Name);
+                        break;
+                    case "!LIMEGREEN":
+                    case "!LIME":
+                        LimeGreenCalc(message.SubMessage, message.Name);
+                        break;
+                    case "!CYAN":
+                        CyanCalc(message.SubMessage, message.Name);
+                        break;
+                    case "!LIGHTBLUE":
+                    case "!SKYBLUE":
+                        LightBlueCalc(message.SubMessage, message.Name);
+                        break;
+                    case "!BLUE":
+                        BlueCalc(message.SubMessage, message.Name);
+                        break;
+                    case "!PURPLE":
+                        PurpleCalc(message.SubMessage, message.Name);
+                        break;
+                    case "!MAGENTA":
+                    case "!LIGHTPURPLE":
+                        MagentaCalc(message.SubMessage, message.Name);
+                        break;
+                    case "!PINK":
+                        PinkCalc(message.SubMessage, message.Name);
+                        break;
+
                 }
             }
         }
@@ -73,16 +126,225 @@ namespace McJoeAdmin.MaterialCalculator
             }
         }
 
+        private void LightGrayCalc(string pParms, string pName)
+        {
+            DoCalculation(pParms, pName, new Action<int>(number =>
+            {
+                var inkSac = (int)Math.Ceiling((float)number / 3);
+                var bonemeal = inkSac * 2;
+
+                SendMessage(string.Format("say {0} Ink sac(s); {1} Bone meal(s)"
+                    , inkSac
+                    , bonemeal));
+            }));
+        }
+
+        private void GrayCalc(string pParms, string pName)
+        {
+            DoCalculation(pParms, pName, new Action<int>(number =>
+            {
+                var inkSac = (int)Math.Ceiling((float)number / 2);
+                var bonemeal = inkSac;
+
+                SendMessage(string.Format("say {0} Ink sac(s); {1} Bone meal(s)"
+                    , inkSac
+                    , bonemeal));
+            }));
+        }
+
+        private void RedCalc(string pParms, string pName)
+        {
+            DoCalculation(pParms, pName, new Action<int>(number =>
+            {
+                var rose = (int)Math.Ceiling((float)number / 2);
+                SendMessage(string.Format("say {0} Rose(s)"
+                    , rose));
+            }));
+        }
+
+        private void OrangeCalc(string pParms, string pName)
+        {
+            DoCalculation(pParms, pName, new Action<int>(number =>
+            {
+                var rose = (int)Math.Ceiling((float)number / 4);
+                SendMessage(string.Format("say {0} Rose(s); {0} Dandelion(s)"
+                    , rose));
+            }));
+        }
+
+        private void YellowCalc(string pParms, string pName)
+        {
+            DoCalculation(pParms, pName, new Action<int>(number =>
+            {
+                var dandelion = (int)Math.Ceiling((float)number / 2);
+                SendMessage(string.Format("say {0} Dandelion(s)"
+                    , dandelion));
+            }));
+        }
+
+        private void LimeGreenCalc(string pParms, string pName)
+        {
+            DoCalculation(pParms, pName, new Action<int>(number =>
+            {
+                var cacti = (int)Math.Ceiling((float)number / 2);
+                SendMessage(string.Format("say {0} Cactus(i); {0} Bonemeal(s)"
+                    , cacti));
+            }));
+        }
+
+        private void GreenCalc(string pParms, string pName)
+        {
+            DoCalculation(pParms, pName, new Action<int>(number =>
+            {
+                //var cacti = (int)Math.Ceiling((float)number / 2);
+                SendMessage(string.Format("say {0} Cactus(i)"
+                    , number));
+            }));
+        }
+
+        private void CyanCalc(string pParms, string pName)
+        {
+            DoCalculation(pParms, pName, new Action<int>(number =>
+            {
+                var lapis = (int)Math.Ceiling((float)number / 2);
+                SendMessage(string.Format("say {0} Cactus(i); {0} Lapis(s)"
+                    , lapis));
+            }));
+        }
+
+        private void LightBlueCalc(string pParms, string pName)
+        {
+            DoCalculation(pParms, pName, new Action<int>(number =>
+            {
+                var lapis = (int)Math.Ceiling((float)number / 2);
+                SendMessage(string.Format("say {0} Lapis(i); {0} Bonemeal(s)"
+                    , lapis));
+            }));
+        }
+        private void BlueCalc(string pParms, string pName)
+        {
+            DoCalculation(pParms, pName, new Action<int>(number =>
+            {
+                SendMessage(string.Format("say {0} Lapis(i); {0} Bonemeal(s)"
+                    , number));
+            }));
+        }
+        private void PurpleCalc(string pParms, string pName)
+        {
+            DoCalculation(pParms, pName, new Action<int>(number =>
+            {
+                var lapis = (int)Math.Ceiling((float)number / 2);
+                var rose = (int)Math.Ceiling((float)number / 4);
+
+                SendMessage(string.Format("say {0} Lapis(i); {1} Rose(s)"
+                    , lapis, rose));
+            }));
+        }
+        private void MagentaCalc(string pParms, string pName)
+        {
+            DoCalculation(pParms, pName, new Action<int>(number =>
+            {
+                var lapis = (int)Math.Ceiling((float)number / 4);
+                var bonemeal = (int)Math.Ceiling((float)number / 4);
+                var rose = (int)Math.Ceiling((float)number / 8);
+
+                SendMessage(string.Format("say {0} Lapis(i); {1} Rose(s); {2} Bonemeal(s)"
+                    , lapis, rose, bonemeal));
+            }));
+        }
+        private void PinkCalc(string pParms, string pName)
+        {
+            DoCalculation(pParms, pName, new Action<int>(number =>
+            {
+                var bonemeal = (int)Math.Ceiling((float)number / 2);
+                var rose = (int)Math.Ceiling((float)number / 4);
+
+                SendMessage(string.Format("say {0} Rose(s); {1} Bonemeal(s)"
+                    , rose, bonemeal));
+            }));
+        }
+
+        private void ArrowsCalc(string pParms, string pName)
+        {
+            DoCalculation(pParms, pName, new Action<int>(number =>
+            {
+
+                var flint = (int)Math.Ceiling((float)number / 4);
+                var feather = flint;
+                var stick = flint;
+                var plank = SticksToPlanks(stick);
+                var log = PlanksToLogs(plank);
+
+                SendMessage(string.Format("say {0} flint(s); {1} feather(s); {2}stick(s) from {3} plank(s) from {4} log(s)"
+                    , flint
+                    , feather
+                    , stick
+                    , plank
+                    , log));
+            }));
+        }
+
+        private void BricksCalc(string pParms, string pName)
+        {
+            DoCalculation(pParms, pName, new Action<int>(number =>
+            {
+                var clayBricks = number * 4;
+                var clay = number * 4;
+
+                SendMessage(string.Format("say {0} Clay Bricks from {1} Clay peices"
+                    , clayBricks
+                    , clay));
+            }));
+        }
+
+        private void LaddersCalc(string pParms, string pName)
+        {
+            DoCalculation(pParms, pName, new Action<int>(number =>
+            {
+                var sticks = number * 7;
+                var planks = SticksToPlanks(sticks);
+                var logs = PlanksToLogs(planks);
+
+                SendMessage(string.Format("say {0} stick(s) from {1} plank(s) from {2} log(s)"
+                    , sticks
+                    , planks
+                    , logs));
+            }));
+        }
+
+        private void FencesCalc(string pParms, string pName)
+        {
+            DoCalculation(pParms, pName, new Action<int>(number =>
+            {
+                var sticks = (int)Math.Ceiling((float)number / 2) * 6;
+                var planks = SticksToPlanks(sticks);
+                var logs = PlanksToLogs(planks);
+
+                SendMessage(string.Format("say {0} stick(s) from {1} plank(s) from {2} log(s)"
+                    , sticks
+                    , planks
+                    , logs));
+            }));
+        }
+
         private void BookshelvesCalc(string pParms, string pName)
         {
             DoCalculation(pParms, pName, new Action<int>(number =>
             {
-                var gunpowder = 5 * number;
-                var sand = 4 * number;
+                var planks = number * 6;
+                var logs = PlanksToLogs(planks);
 
-                SendMessage(string.Format("say {0} gunpowder and {1} sand"
-                    , gunpowder
-                    , sand));
+                var books = number * 3;
+
+                var paper = books * 3;
+                var reeds = (int)Math.Ceiling((float)paper / 3) * 3;
+
+                SendMessage(string.Format("say {0} Planks from {1} logs; {2} books from {3} papers from {4} reeds"
+                    , planks
+                    , logs
+                    , books
+                    , paper
+                    , reeds));
             }));
         }
 
@@ -175,6 +437,6 @@ namespace McJoeAdmin.MaterialCalculator
         {
             return (int)Math.Ceiling((float)pPlanks / 4);
         }
-
+        
     }
 }
